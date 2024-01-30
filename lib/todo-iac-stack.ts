c583d5072957358905069271e80d241bb80bc737
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-29 20:08:00 -0500

    Initial cdk stack to deploy Ec2 instance for app

--- lib/todo-iac-stack.ts
+++ lib/todo-iac-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { AmazonLinuxImage, Instance, InstanceType, Peer, Port, SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
 
 export class TodoIacStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a VPC
+    const vpc = new Vpc(this, 'ExpressTodoAppVpc', {
+      maxAzs: 2, // Set the number of Availability Zones as needed
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TodoIacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a security group
+    const securityGroup = new SecurityGroup(this, 'ExpressTodoAppSecurityGroup', {
+      vpc,
+    });
+
+    // Allow inbound traffic on port 80 (or your desired port)
+    securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(80));
+
+    // Launch EC2 instance
+    const instance = new InstanceType('t2.micro');
+    const machineImage = new AmazonLinuxImage();
+
+    const ec2Instance = new Instance(this, 'ExpressTodoAppInstance', {
+      instanceType: instance,
+      machineImage,
+      vpc,
+      securityGroup,
+    });
   }
 }
