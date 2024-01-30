8eddf26991350d72144dfc15fd3466b04b68a12a
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-29 20:31:19 -0500

    Get docker image from s3 bucket to run in Ec2 instance

--- lib/todo-iac-stack.ts
+++ lib/todo-iac-stack.ts
@@ -1,23 +1,36 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { AmazonLinuxImage, Instance, InstanceType, Peer, Port, SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { AmazonLinuxImage, CloudFormationInit, Instance, InstanceType, Peer, Port, SecurityGroup, UserData, Vpc } from 'aws-cdk-lib/aws-ec2';
 
 export class TodoIacStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create a VPC
     const vpc = new Vpc(this, 'ExpressTodoAppVpc', {
-      maxAzs: 2, // Set the number of Availability Zones as needed
+      maxAzs: 2,
     });
 
-    // Create a security group
     const securityGroup = new SecurityGroup(this, 'ExpressTodoAppSecurityGroup', {
       vpc,
     });
 
-    // Allow inbound traffic on port 80 (or your desired port)
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(80));
+
+    const s3BucketName = this.node.tryGetContext('ExpressTodoAppS3BucketName');
+
+    const userData = UserData.forLinux();
+    userData.addCommands(
+      'yum update -y',
+      'yum install -y amazon-efs-utils',
+      'yum install -y wget',
+      'wget https://s3.amazonaws.com/mountpoint-s3-release/latest/x86_64/mount-s3.rpm -O /tmp/mount-s3.rpm',
+      'yum localinstall -y /tmp/mount-s3.rpm',
+      'rm /tmp/mount-s3.rpm',
+      `mount-s3 ${s3BucketName} /mnt`,
+      'ls /mnt/s3',
+      'docker load -i /mnt/todo-express-image-1_0_0.tar.gz',
+      'docker run -p 80:3001 -d todo-express:1.0.0',
+    );
 
     // Launch EC2 instance
     const instance = new InstanceType('t2.micro');
