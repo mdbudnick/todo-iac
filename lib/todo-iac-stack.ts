f3bc5ebd3e661fbc800a038b1ec14666221e2d9a
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-02-05 18:02:52 -0500

    Deploying and starting service without root privileges

--- lib/todo-iac-stack.ts
+++ lib/todo-iac-stack.ts
@@ -14,27 +14,33 @@ export class TodoIacStack extends cdk.Stack {
       vpc,
     });
 
-    securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(80));
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(443));
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22));
-
     const machineImage = MachineImage.latestAmazonLinux2023()
     const userData = UserData.forLinux();
     userData.addCommands(
+      'chown -R ec2-user: /home/ec2-user',
+      'export HOME=/home/ec2-user',
+      'cd /home/ec2-user',
       'yum update -y',
+      'sudo -u ec2-user bash -c "whoami;PATH=$PATH:/usr/local/bin"',
       'yum install -y git',
       'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash',
       '. ~/.nvm/nvm.sh',
       'nvm install --lts',
-      'setcap \'cap_net_bind_service=+ep\' $(which node)',
-      'npm install -g pm2',
-      'export APP_PORT=80',
+      'npm install pm2@latest -g',
+      'sudo chown -R ec2-user:ec2-user "/home/ec2-user/.npm"',
       'git clone https://github.com/mdbudnick/todo-express.git -b prod',
+      'sudo chown -R ec2-user: /home/ec2-user/todo-express',
       'cd todo-express',
       'openssl genrsa -out privatekey.pem',
       'openssl req -new -x509 -key privatekey.pem -out server.crt -days 365 -subj "/C=US/ST=NY/L=NYC/O=Waves Workshop/CN=ec2-todo/emailAddress=michaeldbudnick@gmail.com"',
-      'npm ci',
-      'pm2 start npm --name "todo-express" -- start',
+      'NODE_ENV=production npm ci --include=dev > /tmp/npm.log 2>&1',
+      'export APP_PORT=443',
+      'sudo setcap "cap_net_bind_service=+ep" $(which node)',
+      'pm2 start npm --name "todo-express" -- start > /tmp/pm2.log 2>&1',
+      'sudo chown ec2-user:ec2-user /home/ec2-user/.pm2/rpc.sock /home/ec2-user/.pm2/pub.sock',
+      'touch /tmp/complete.log'
     );
     const instance = new InstanceType('t2.micro');
     const keyPair = KeyPair.fromKeyPairName(this, 'EC2-KeyPair', 'ec2-todo')
@@ -46,6 +52,16 @@ export class TodoIacStack extends cdk.Stack {
       securityGroup,
       vpcSubnets: { subnetType: SubnetType.PUBLIC },
       keyPair
+    });
+    
+    new cdk.CfnOutput(this, 'InstancePublicIp', {
+      value: ec2Instance.instancePublicIp,
+      description: 'Public IPv4 address of the EC2 instance',
+    });
+
+    new cdk.CfnOutput(this, 'InstancePublicTasksEndpoint', {
+      value: "https://" + ec2Instance.instancePublicDnsName + "/v1/tasks",
+      description: 'API endpoint of the EC2 instance',
     });
   }
 }
